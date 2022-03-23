c273eb79bfc32eb800db33965f8d3a5077c8b6e3
ryunianti <ryunianti30@gmail.com'=>
2022-03-23 23:48:18 +0700

    Export Master Data Pegawai

--- utama/admin/exportexcel/exportexcel_pegawai.php
+++ utama/admin/exportexcel/exportexcel_pegawai.php
@@ -32,30 +32,39 @@ readfile('../tittle.php');
  
 	<?php
 	header("Content-type: application/vnd-ms-excel");
-	header("Content-Disposition: attachment; filename=Data masterbarang.xls");
+	header("Content-Disposition: attachment; filename=Data masterdatapegawai.xls");
 	?>
  
 	<center>
-		<h1>Data <br/>Barang</h1>
+		<h1>Data <br/>Pegawai</h1>
 	</center>
  
 	<table border="1">
 		<tr>
 			<th>No</th>
 			<th>Nama</th>
+			<th>Alamat KTP</th>
+			<th>Alamat Domisili</th>
+			<th>Status Karyawan</th>
+			<th>Masa Kontrak</th>
 		</tr>
 		<?php 
 		// koneksi database
 		$koneksi = mysqli_connect("Localhost","root","","kasir");
  
 		// menampilkan data pegawai
-		$data = mysqli_query($koneksi,"select * from masterdata_barang");
+		$data = mysqli_query($koneksi,"select * from masterdata_pegawai");
 		$no = 1;
 		while($d = mysqli_fetch_array($data)){
 		?>
 		<tr>
 			<td><?php echo $no++; ?></td>
 			<td><?php echo $d['nama']; ?></td>
+			<td><?php echo $d['alamat_ktp']; ?></td>
+			<td><?php echo $d['alamat_domisili']; ?></td>
+			<td><?php echo $d['status_karyawan']; ?></td>
+			<td><?php echo $d['masa_kontrak']; ?></td>
+
 		</tr>
 		<?php 
 		}
